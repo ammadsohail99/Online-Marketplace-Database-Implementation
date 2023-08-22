c9b2075095651f34b28fdc7fb7cc251670b035f2
krishangupta33 <k.gupta035@gmail.com>
2023-08-22 14:41:48 -0400

    Update sql.py

--- sql.py
+++ sql.py
@@ -4,8 +4,25 @@ from sqlalchemy import create_engine
 import toml
 
 query_dict = {1:"SELECT * FROM Tasker",
-              2:"SELECT * FROM Tasker limit 2"
-
+              2:"SELECT * FROM Tasker ",
+              3:"SELECT * FROM Tasker ",
+              4:"SELECT * FROM Tasker ",
+              5:"SELECT * FROM Tasker ",
+              6:"SELECT * FROM Tasker ",
+              7:"SELECT * FROM Tasker ",
+              8:"SELECT * FROM Tasker ",
+              9:"SELECT * FROM Tasker ",
+              10:"SELECT * FROM Tasker ",
+              11:"SELECT * FROM Tasker ",
+              12:"SELECT * FROM Tasker ",
+              13:"SELECT * FROM Tasker ",
+              14:"SELECT * FROM Tasker ",
+              15:"SELECT * FROM Tasker ",
+              16:"SELECT * FROM Tasker ",
+              17:"SELECT * FROM Tasker ",
+              18:"SELECT * FROM Tasker ",
+              19:"SELECT * FROM Tasker ",
+              20:"SELECT * FROM Tasker "
               }
 
 #connect with an sql database
