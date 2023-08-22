e225923698faf339446446d181fd7828de52fe41
krishangupta33 <k.gupta035@gmail.com>
2023-08-22 14:33:47 -0400

    Update sql.py

--- sql.py
+++ sql.py
@@ -3,15 +3,20 @@ import pandas as pd
 from sqlalchemy import create_engine
 import toml
 
+query_dict = {1:"SELECT * FROM Tasker"
+              2:"SELECT * FROM Tasker WHERE TaskerID = 1"
+
+              }
+
 #connect with an sql database
 secrets = toml.load("secrets.toml")
 
 # Connect to SQL database
 engine = create_engine(f"mssql+pyodbc://{secrets['azure']['username']}:{secrets['azure']['password']}@{secrets['azure']['server']}/{secrets['azure']['database']}?driver=ODBC+Driver+17+for+SQL+Server")
 
-query = "SELECT * FROM Tasker"
 
-
-df = pd.read_sql(query, con=engine)
-st.write(df)
+for i in range(1, len(query_dict)+1):
+    st.write(f"Query {i}")
+    query = query_dict[i]
+    st.write(pd.read_sql(query, con=engine))
    
